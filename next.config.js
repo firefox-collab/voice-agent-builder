904cc77dc5adf95ba06374c1c6b422ebb49dde50
aicitsolutionsph-a11y <fire29fox@gmail.com>
2025-11-26 19:57:46 +0800

    Fix: Resolve merge conflict, remove corrupted favicon

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD:next.config.js
-$nextConfig = '/** @type {import(''next'').NextConfig} */
-=======
 /** @type {import('next').NextConfig} */
->>>>>>> e6ade6207d66aba83e1d36fe65d1847579fd933e:voice-agent-builder/next.config.js
 const nextConfig = {
   /* config options here */
 };
 
-<<<<<<< HEAD:next.config.js
-module.exports = nextConfig;'
-=======
 module.exports = nextConfig;
->>>>>>> e6ade6207d66aba83e1d36fe65d1847579fd933e:voice-agent-builder/next.config.js
